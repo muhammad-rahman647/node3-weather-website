eca91b2917a70749e15c73eb48659f77315e29f6
muhammad-rahman647 <rahmansheikh718@gmail.com>
2019-11-13 16:06:12 +0500

    Removes unnecessary console log from file

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,21 @@
-console.log('Client side javascript is running');
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
 
-const weatherForm = document.querySelector('form');
-const search = document.querySelector('input');
-const messageOne = document.querySelector('#message-1');
-const messageTwo = document.querySelector('#message-2');
-
-weatherForm.addEventListener('submit', e => {
+weatherForm.addEventListener("submit", e => {
   e.preventDefault();
 
   const location = search.value;
 
-  messageOne.textContent = 'Loading...';
-  messageTwo.textContent = '';
+  messageOne.textContent = "Loading...";
+  messageTwo.textContent = "";
 
   fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
     response.json().then(data => {
       if (data.error) {
         messageOne.textContent = data.error;
-        messageTwo.textContent = '';
+        messageTwo.textContent = "";
       } else {
         messageOne.textContent = data.location;
         messageTwo.textContent = data.forecast;
